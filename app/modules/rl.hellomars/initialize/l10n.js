86dbba9c6d632de02ae1f83c554072bc9542c7d3
Tamara Temple <tamouse@gmail.com>
2015-03-30 13:51:46 -0500

    copy helloword => hellomars

---
+++ app/modules/rl.hellomars/initialize/l10n.js
@@ -0,0 +1,13 @@
+angular.rlmodule('rl.helloworld.initialize.L10n', ['rl.l10n'])
+  .run(function (LocaleSettings) {
+    // Set your initial locale
+    LocaleSettings.locale('en');
+
+    // Set your default locale
+    // If we can't find a translation in your selected locale,
+    // we'll look for it using this locale - this is a fallback
+    LocaleSettings.defaultLocale('en');
+
+    // What locales are available?
+    LocaleSettings.locales(['en', 'pt']);
+  });
